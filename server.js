5b5f229a5a90096ce5fb00881e137ba08c82c7d5
Gabriel <gabriel_luchtenberg@edu.univali.br>
2018-11-02 14:13:21 -0300

    alteracao da porta default do servidor

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
-// server.js
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
+const express = require('express');
+const serveStatic = require("serve-static")
+const path = require('path');
 app = express();
-app.use(serveStatic(__dirname + "/dist"));
-var port = process.env.PORT || 8080;
+app.use(serveStatic(path.join(__dirname, 'dist')));
+const port = process.env.PORT || 80;
 app.listen(port);
-console.log('server started ' + port);
