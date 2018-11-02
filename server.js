11ba2348d4b1a4bee5fa74b79556de6437054a17
Gabriel <gabriel_luchtenberg@edu.univali.br>
2018-11-02 14:29:17 -0300

    added express to dependencies

--- server.js
+++ server.js
@@ -3,5 +3,4 @@ const serveStatic = require("serve-static")
 const path = require('path');
 app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
-
 app.listen(process.env.PORT || 3000);
